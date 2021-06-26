c7a7fc24caa03cd1d3a22613155af69919c48d8d
Sumeet <sumeet13gupta@gmail.com>
2021-06-26 17:42:05 +0530

    config controls the difficulty and mine rate values for now

--- blockchain/block.test.js
+++ blockchain/block.test.js
@@ -1,4 +1,6 @@
 const Block =  require(('./block'));
+const { DIFFICULTY } =  require('../config');
+
 
 describe('Block', () => {
     let data, lastBlock, block;     // if the variables are not described here , we will have to take const inside the beforeaEach function
@@ -15,6 +17,13 @@ describe('Block', () => {
     it('sets the `lastHash` to match the hah of the last block', () => {
         expect(block.lastHash).toEqual(lastBlock.hash);       // this is to check the lastHash of the block and then it matches it with the hash value of the last block to which it should actually be equal to.
     });
+
+    it('generates a hash that matches the difficulty', () => {
+        expect(block.hash.substring(0,DIFFICULTY)).toEqual('0'.repeat(DIFFICULTY));
+        console.log(block.toString());
+    });
+
+
 });
 
 // beforeEach() funchtion helps to run the same code before each of the following unit tests
